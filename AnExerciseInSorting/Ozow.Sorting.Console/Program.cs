e35c66e8c1beeb17b09723065f30adc93c98be73
Keagan Phillips <keaganp@gmail.com>
2019-04-15 20:11:40 +0200

    Add initial project structure for sorting app

---
+++ AnExerciseInSorting/Ozow.Sorting.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Ozow.Sorting.Console
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
